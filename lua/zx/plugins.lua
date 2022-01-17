b00b0d24816aed407e0f0309190ad6b9c7ed557a
FZRKexEr <wrong.answer@qq.com>
2022-01-17 19:28:39 +0800

    Update

--- lua/zx/plugins.lua
+++ lua/zx/plugins.lua
@@ -28,7 +28,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     ]])
     vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
-    return M
   end
   vim.fn['plug#begin']()
   for i = 1, #plugins do 
