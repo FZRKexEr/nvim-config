0236d7a2ab4e06cd85979c937214bd6acdb5fc63
FZRKexEr <wrong.answer@qq.com>
2022-04-04 15:10:09 +0800

    Update

--- lua/configs/nvim-treesitter.lua
+++ lua/configs/nvim-treesitter.lua
@@ -1,5 +1,6 @@
 require('nvim-treesitter.configs').setup {
   ensure_installed = "maintained",
+--  ensure_installed = {"cpp", "c", "python", "bash", "lua", "vim"},
   sync_install = false,
   highlight = {
     enable = true,
