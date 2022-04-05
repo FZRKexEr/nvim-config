ccb23d3895f17d6f18499c25ac7d2b657d025578
FZRKexEr <wrong.answer@qq.com>
2022-04-06 01:44:07 +0800

    Update

--- install-cn.sh
+++ install-cn.sh
@@ -4,5 +4,5 @@ mv ~/.config/nvim ~/.config/nvim.bak
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://ghproxy.com/https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 git clone https://ghproxy.com/https://github.com/FZRKexEr/nvim-config.git ~/.config/nvim/
-echo "vim.g.plug_url_format='https://ghproxy.com/https://github.com/%s'" >> ~/.config/nvim/init.lua
+echo "let g:plug_url_format='https://ghproxy.com/https://github.com/%s'" >> ~/.config/nvim/init.vim
 
