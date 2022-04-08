984402c40b9a62df5cb8c8c40161f976edf8bbff
LiZnB <51793302+FZRKexEr@users.noreply.github.com>
2022-04-09 01:06:56 +0800

    Update install-cn.sh

--- install-cn.sh
+++ install-cn.sh
@@ -2,13 +2,13 @@ rm -rf ~/.config/nvim.bak
 rm -rf ~/.local/share/nvim/
 mv ~/.config/nvim ~/.config/nvim.bak
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://ghproxy.com/https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-git clone https://ghproxy.com/https://github.com/FZRKexEr/nvim-config.git ~/.config/nvim/
-echo "let g:plug_url_format='https://ghproxy.com/https://github.com/%s'" >> ~/.config/nvim/init.vim
+       https://github.wrong-answer.workers.dev/junegunn/vim-plug/master/plug.vim'
+git clone https://github.wrong-answer.workers.dev/FZRKexEr/nvim-config.git ~/.config/nvim/
+echo "let g:plug_url_format='https://github.wrong-answer.workers.dev/%s'" >> ~/.config/nvim/init.vim
 cat > ~/.config/nvim/lua/configs/nvim-treesitter.lua <<EOF
 require("nvim-treesitter.install").prefer_git = true
 for _, config in pairs(require("nvim-treesitter.parsers").get_parser_configs()) do
-  config.install_info.url = config.install_info.url:gsub("https://ghproxy.com/https://github.com/", "something else")
+  config.install_info.url = config.install_info.url:gsub("https://github.wrong-answer.workers.dev/", "something else")
 end
 
 require('nvim-treesitter.configs').setup {
