0d6f9889ed8c4605cecbf58c466a5a02bc381d2f
FZRKexEr <wrong.answer@qq.com>
2022-04-09 18:31:09 +0800

    Update

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ mv ~/.config/nvim ~/.config/nvim.bak
 git clone https://github.com/FZRKexEr/nvim-config.git ~/.config/nvim/
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://ghproxy.com/https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
