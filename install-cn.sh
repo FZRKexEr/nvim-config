1b1a09d19accad0762c37549108af39148a2a34b
LiZnB <51793302+FZRKexEr@users.noreply.github.com>
2022-04-09 17:50:37 +0800

    Update install-cn.sh

--- install-cn.sh
+++ install-cn.sh
@@ -2,8 +2,6 @@ rm -rf ~/.config/nvim.bak
 rm -rf ~/.local/share/nvim/
 mv ~/.config/nvim ~/.config/nvim.bak
 
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://github-raw.wrong-answer.workers.dev/junegunn/vim-plug/master/plug.vim'
 
 git clone https://github.wrong-answer.workers.dev/FZRKexEr/nvim-config.git ~/.config/nvim/
 
@@ -24,3 +22,6 @@ require('nvim-treesitter.configs').setup {
   },
 }
 EOF
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://github-raw.wrong-answer.workers.dev/junegunn/vim-plug/master/plug.vim'
