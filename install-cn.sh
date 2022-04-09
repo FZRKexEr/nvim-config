ed2cf84f795af298dce8538fda94c5cd0cedad37
LiZnB <51793302+FZRKexEr@users.noreply.github.com>
2022-04-09 17:43:56 +0800

    Update install-cn.sh

--- install-cn.sh
+++ install-cn.sh
@@ -3,7 +3,7 @@ rm -rf ~/.local/share/nvim/
 mv ~/.config/nvim ~/.config/nvim.bak
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://github.wrong-answer.workers.dev/junegunn/vim-plug/master/plug.vim'
+       https://github-raw.wrong-answer.workers.dev/junegunn/vim-plug/master/plug.vim'
 
 git clone https://hub.fastgit.xyz/FZRKexEr/nvim-config.git ~/.config/nvim/
 
